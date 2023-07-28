2efe462dae2aa47d26dea97e8dd4189108e37e52
thang-39 <91838559+thang-39@users.noreply.github.com>
2023-07-28 21:38:43 +0700

    buoi 05

--- buoi03/5.10 Exercise.R
+++ buoi03/5.10 Exercise.R
@@ -10,30 +10,55 @@
 #
 # [[2]][[2]]
 # [1] "Archery" 
+list <- list(c(1,3,5,7,9,11),list("Happy Birthday","Archery"))
 
+list
 
 # extract the numbers as a vector
+vector1 <- list[[1]]
 
 # extract the phrase Happy Birthday as a vector
+hb <- list[[2]][[1]]
+
 
 # extract the second item of the second list as a list
+list[[2]][2]
+
 
 # extract the second list as a list
+list[[2]]
 
 # extract the numbers item as a list
+list[1]
 
 # add 2 to each element in the numbers item
+list[[1]]+2
+
 
 # name the items in the list as "Numbers" and "Phrases"
+list <- list(Number = c(1,3,5,7,9,11),
+             Phrases = list("Happy Birthday","Archery"))
+list
+
 
 # you can use the $ to extract named items of a list
 # if you extract the numbers item from newList with the $, what other extraction method is this equivalent to?
+list$Number
+list[[1]]
 
-# use the dollar sign to repeat the addition from above (add 2 to each element in the numbers list)
+# use the dollar sign to repeat the addition from above 
+# (add 2 to each element in the numbers list)
+list$Number + 2
 
 # add a new item called "Brands" to the list. It should contain the brands Kellogs, Nike, iPhone
 # use either brackets or the dollar sign to do that
-
+list[["Brands"]] <- c("Kellogs", "Nike", "iPhone")
+list$Brands <- c("Kellogs", "Nike", "iPhone")
+list
 # remove the iPhone from the Brands item
-
+list$Brands <- list$Brands[list$Brands != "iPhone"]
+list$Brands <- list$Brands[-3]
+list
 # remove the Brands item from the list
+list <- list[-3]
+list
---
+++ buoi05/6.3 Exercise.R
@@ -0,0 +1,10 @@
+
+
+# Write a for loop that sums all the numbers from 1 to n
+n <- 10
+sum <- 0
+for(i in 1:n) {
+  sum = sum + i;
+}
+
+
